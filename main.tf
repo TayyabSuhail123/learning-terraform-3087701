13003069dae409fb1ce6e8e84a397cd7e187f612
TayyabSuhail123 <tayyeb.suhail@gmail.com>
2025-03-28 01:41:10 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "blog" {
   vpc_id      = data.aws_vpc.default.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type      = "ingress"
   from_port = 80
   to_port   = 80
@@ -46,7 +46,7 @@ resource "aws_vpc_security_group_ingress_rule" "blog_http_in" {
 
 }
 
-resource "aws_vpc_security_group_ingress_rule" "blog_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type      = "ingress"
   from_port = 443
   to_port   = 443
@@ -57,7 +57,7 @@ resource "aws_vpc_security_group_ingress_rule" "blog_https_in" {
 
 }
 
-resource "aws_vpc_security_group_ingress_rule" "blog_everything_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type      = "egress"
   from_port = 0
   to_port   = 0
